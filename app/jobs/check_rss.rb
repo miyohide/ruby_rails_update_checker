d56f509aaf8de3f307846440a6df5836be841390
Hidenori Miyoshi <miyohide@gmail.com>
2013-12-11 22:15:59 +0900

    delete test method. and write rss check function.

--- app/jobs/check_rss.rb
+++ app/jobs/check_rss.rb
@@ -3,17 +3,15 @@ require 'rss'
 module CheckRss
    @queue = :check_rss_queue
    def self.perform
-      # rails_rss = RSS::Parser.parse('http://weblog.rubyonrails.org/feed/atom.xml')
-      # max_rss_updated_at = RssEntry.maximum('entry_updated_at')
-      # rails_rss.items.each do |item|
-      #    if max_rss_updated_at.nil? || item.updated.content > max_rss_updated_at
-      #       RssEntry.create(
-      #          title: item.title.content,
-      #          entry_updated_at: item.updated.content
-      #       )
-      #    end
-      # end
-
-      puts "Doing my job"
+      rails_rss = RSS::Parser.parse('http://weblog.rubyonrails.org/feed/atom.xml')
+      max_rss_updated_at = RssEntry.maximum('entry_updated_at')
+      rails_rss.items.each do |item|
+         if max_rss_updated_at.nil? || item.updated.content > max_rss_updated_at
+            RssEntry.create(
+               title: item.title.content,
+               entry_updated_at: item.updated.content.to_datetime
+            )
+         end
+      end
    end
 end
