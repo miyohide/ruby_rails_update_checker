dbc75d7734fa1d0953edb8d93a7dc4c64a7f384a
Hidenori Miyoshi <miyohide@gmail.com>
2013-12-12 22:06:18 +0900

    Use Feedzirra.

--- app/jobs/check_rss.rb
+++ app/jobs/check_rss.rb
@@ -1,15 +1,13 @@
-require 'rss'
-
 module CheckRss
    @queue = :check_rss_queue
    def self.perform
-      rails_rss = RSS::Parser.parse('http://weblog.rubyonrails.org/feed/atom.xml')
+      rails_rss = Feedzirra::Feed.fetch_and_parse('http://weblog.rubyonrails.org/feed/atom.xml')
       max_rss_updated_at = RssEntry.maximum('entry_updated_at')
-      rails_rss.items.each do |item|
-         if max_rss_updated_at.nil? || item.updated.content > max_rss_updated_at
+      rails_rss.entries.each do |entry|
+         if max_rss_updated_at.nil? || entry.updated.to_datetime > max_rss_updated_at
             RssEntry.create(
-               title: item.title.content,
-               entry_updated_at: item.updated.content.to_datetime
+               title: entry.title,
+               entry_updated_at: entry.updated.to_datetime
             )
          end
       end
