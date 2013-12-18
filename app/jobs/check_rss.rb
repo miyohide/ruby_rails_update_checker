8c458f507b0d80a3fff8167bb0452175d60dc052
Hidenori Miyoshi <miyohide@gmail.com>
2013-12-18 21:38:44 +0900

    複数のリソースを読めるように対応

--- app/jobs/check_rss.rb
+++ app/jobs/check_rss.rb
@@ -1,16 +1,27 @@
 module CheckRss
    @queue = :check_rss_queue
    def self.perform
-      rails_rss = Feedzirra::Feed.fetch_and_parse('http://weblog.rubyonrails.org/feed/atom.xml')
-      max_rss_updated_at = RssEntry.with_deleted.maximum(:entry_updated_at)
-      rails_rss.entries.each do |entry|
+      rss_list = { rails: 'http://weblog.rubyonrails.org/feed/atom.xml',
+                   ruby:  'https://www.ruby-lang.org/ja/feeds/news.rss' }
+
+      rss_list.keys.each do |name|
+         rss_entry_update(name, rss_list[name])
+      end
+   end
+
+   def rss_entry_update(name, url)
+      rss = Feedzirra::Feed.fetch_and_parse(url)
+      max_rss_updated_at = RssEntry.with_deleted.where(name: name).maximum(:entry_updated_at)
+      rss.entries.each do |entry|
          if max_rss_updated_at.nil? || entry.updated.to_datetime > max_rss_updated_at
             RssEntry.create(
                title: entry.title,
-               content: entry.content.gsub(/<.+?>/m, ''),
-               entry_updated_at: entry.updated.to_datetime
+               content: entry.content.try(:sanitize),
+               entry_updated_at: entry.published.to_datetime,
+               name: name
             )
          end
       end
    end
+   module_function :rss_entry_update
 end
