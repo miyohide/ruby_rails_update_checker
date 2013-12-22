8d59c99c83b73032b9388fbbfe7adb48aa29a9fa
Hidenori Miyoshi <miyohide@gmail.com>
2013-12-22 23:47:24 +0900

    create package action.

--- app/controllers/rss_entries_controller.rb
+++ app/controllers/rss_entries_controller.rb
@@ -7,6 +7,13 @@ class RssEntriesController < ApplicationController
      @rss_entry = RssEntry.find(params[:id])
   end
 
+  def package
+     rss_entry = RssEntry.find(params[:id])
+     fname = "rss_entry_#{rss_entry.id}_#{DateTime.now.to_i}.txt"
+     send_data rss_entry.package, type: 'text/text;',
+        disposition: "attachment; filename=#{fname}"
+  end
+
   def destroy
      RssEntry.find(params[:id]).destroy
      redirect_to rss_entries_url
--- app/models/rss_entry.rb
+++ app/models/rss_entry.rb
@@ -1,3 +1,7 @@
 class RssEntry < ActiveRecord::Base
    acts_as_paranoid
+
+   def package
+      self.content
+   end
 end
