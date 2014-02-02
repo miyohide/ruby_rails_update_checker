333c053339b03474fee286c095f25b566f844143
Hidenori Miyoshi <miyohide@gmail.com>
2014-02-02 21:32:11 +0900

    send_address画面を表示させるようにした。

--- app/controllers/settings/send_addresses_controller.rb
+++ app/controllers/settings/send_addresses_controller.rb
@@ -1,4 +1,6 @@
 class Settings::SendAddressesController < ApplicationController
    def index
+      @send_addresses = Settings.unscoped.where(var: 'send_addresses')
+      @send_addresses = Settings.send_addresses if @send_addresses.empty?
    end
 end
