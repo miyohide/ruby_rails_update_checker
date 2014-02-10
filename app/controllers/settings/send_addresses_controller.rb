2b58a65ed44761e03b50686fbf6cd1d2b903ea29
Hidenori Miyoshi <miyohide@gmail.com>
2014-02-10 09:40:09 +0900

    send addressのnew画面を作成

--- app/controllers/settings/send_addresses_controller.rb
+++ app/controllers/settings/send_addresses_controller.rb
@@ -3,4 +3,7 @@ class Settings::SendAddressesController < ApplicationController
       @send_addresses = Settings.unscoped.where(var: 'send_addresses')
       @send_addresses = Settings.send_addresses if @send_addresses.empty?
    end
+
+   def new
+   end
 end
