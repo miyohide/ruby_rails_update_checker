d202ecd8c3fdf14f80ca5aa8974f06d9ce69a054
Hidenori Miyoshi <miyohide@gmail.com>
2014-02-10 22:07:24 +0900

    送信先の追加処理の実装

--- app/controllers/settings/send_addresses_controller.rb
+++ app/controllers/settings/send_addresses_controller.rb
@@ -1,9 +1,20 @@
 class Settings::SendAddressesController < ApplicationController
    def index
-      @send_addresses = Settings.unscoped.where(var: 'send_addresses')
-      @send_addresses = Settings.send_addresses if @send_addresses.empty?
+      saved_data = Settings.unscoped.where(var: 'send_addresses')
+      if saved_data.empty?
+         @send_addresses = Settings.send_addresses
+      else
+         @send_addresses = saved_data.first
+      end
    end
 
    def new
+   end
+
+   def create
+      input_address = Hash[:to, params[:To], :name, params[:Name]]
+      puts input_address.inspect
+      Settings.send_addresses = Settings.send_addresses << input_address
+      redirect_to action: :index
    end
 end
