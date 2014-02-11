5aa1d7db3e1bb5fc3b4f6de455fb3510590c97ec
Hidenori Miyoshi <miyohide@gmail.com>
2014-02-12 01:15:25 +0900

    SettingsLogicを使って設定情報を持たせるようにした。

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,2 +1,5 @@
-class Settings < RailsSettings::CachedSettings
+class Settings < Settingslogic
+   source "#{Rails.root}/config/application_settings.yml"
+   namespace Rails.env
 end
+
--- config/initializers/default_settings.rb
+++
@@ -1,9 +0,0 @@
-Settings.defaults[:rss_urls] = {
-   rails: "http://weblog.rubyonrails.org/feed/atom.xml",
-   ruby:  "https://www.ruby-lang.org/ja/feeds/news.rss"
-}
-
-Settings.defaults[:send_addresses] = [
-   { to: "foo.bar@example.com", name: "foo.barさま" }
-]
-
