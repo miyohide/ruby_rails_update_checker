0272d34aa1486b30a45301f660d8b85026e2e0a6
Hidenori Miyoshi <miyohide@gmail.com>
2014-03-28 22:19:40 +0900

    Implement ruby version check.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,19 @@ class Project < ActiveRecord::Base
    validates :name, presence: true
    validates :person_in_charge, presence: true
    validates :contact_address, presence: true
+
+   def check_ruby_version(target_version)
+      return 0 if ruby_version == target_version
+      target_version_num = Project.version_to_number(target_version)
+      self_version_num = Project.version_to_number(self.ruby_version)
+
+      return target_version_num > self_version_num ? 1 : -1
+
+   end
+
+   def self.version_to_number(version)
+      version.split(".").each_with_index.
+         map { |x, i| x.to_i * 10 ** (1 - i)}.
+         inject(0) { |sum, n| sum + n }
+   end
 end
